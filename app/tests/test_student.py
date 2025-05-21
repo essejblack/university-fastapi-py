0792a01fcff5da70ab1d34633b2a89478257b8c4
Ehsan Jahanara <ehsan.jnra@gmail.com>
2025-05-21 12:51:04 +0330

    use Mock for test to prevent real data insertion

--- app/tests/test_student.py
+++ app/tests/test_student.py
@@ -1,13 +1,30 @@
 import pytest
 from fastapi.testclient import TestClient
+from unittest.mock import MagicMock
+from random import randint
+
 from app.main import app
+from app.database.db import get_db
+from app.models.student import Student
 
 @pytest.fixture(scope="module")
-def client():
+def override_get_db():
+    mock_db = MagicMock()
+    def fake_add(student_obj):
+        student_obj.id = 123
+
+    mock_db.add.side_effect = fake_add
+    mock_db.commit.return_value = None
+    mock_db.refresh.side_effect = lambda obj: None
+
+    yield mock_db
+
+@pytest.fixture(scope="module")
+def client(override_get_db):
+    app.dependency_overrides[get_db] = lambda: override_get_db
     with TestClient(app) as c:
         yield c
-
-from random import randint
+    app.dependency_overrides.clear()
 
 def test_create_student_from_username(client):
     random = randint(1, 1000)
